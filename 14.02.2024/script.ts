18a3febc33e10854ece969d40d7a51ba385c994f
Солосин Макар <itami@stud.vvsu.ru>
2024-02-14 18:06:29 +1000

    add

---
+++ 14.02.2024/script.ts
@@ -0,0 +1,39 @@
+// class Temp{ 
+//     private x: number 
+//     public y: number 
+//     constructor(val1?: number,val2?: number){ 
+//         val1 ? this.x = val1 : this.x = 1 
+//         val2 ? this.y = val2 : this.y = 2 
+//     } 
+ 
+ 
+// get getX(){ 
+//     return this.x 
+// } 
+// set setX(value: number){ 
+//     this.x = value 
+// } 
+// } 
+// let temp2 = new Temp(10,20)
+// temp2.setX = 6
+// console.log(temp2.getX)
+
+class Point {
+    x: number
+    y: number
+    constructor(vall: number, val2: number) {
+        this.x = vall
+        this.y = val2
+    }
+}
+class Triangle {
+    x: Point
+    y: Point
+    z: Point
+    constructor(x1?: number, y1?: number, x2?: number, y2?: number, x3?: number, y3?: number) {
+    }
+}
+
+let pointl: Point = new Point(5,6)
+let tr0: Triangle = new Triangle()
+let tr1: Triangle = new Triangle(1,2,3,4,5,6)
